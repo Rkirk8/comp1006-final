3bb966331dfd2046084a9fb481f51c9aa02f9edb
Rielly Kirk <Riellykirk13@gmail.com>
2024-04-16 10:01:37 -0400

    finished the header and the test (thank you for an amazing semester)

--- include/header.php
+++ include/header.php
@@ -11,7 +11,8 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">PHP GAMING</a>
+        <!-- replace "PHP GAMING" with uploaded logo -->
+        <a class="navbar-brand" href="index.php"><img src="logo.png" /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
